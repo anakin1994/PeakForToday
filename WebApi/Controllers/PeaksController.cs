25df09da918b74baa666afaccdc41e2364afe1c1
Piotr Markowski <pimarkow@microsoft.com>
2019-03-25 13:05:25 -0700

    Peaks controller

---
+++ WebApi/Controllers/PeaksController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+using WebApi.Model;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeaksController : Controller
+    {
+        [HttpGet("[action]")]
+        public IEnumerable<Peak> PeaksInRadius(double latitude, double longitude, int radiusKm)
+        {
+            var allPeaks = PeaksDataProvider.GetAllPeaks();
+            return allPeaks.Where(p => p.GetDistanceToKm(latitude, longitude) <= radiusKm);
+        }
+    }
+}
---
+++ WebApi/Data/PeaksDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WebApi.Model;
+
+namespace WebApi.Data
+{
+    public static class PeaksDataProvider
+    {
+        private static List<Peak> _allPeaks;
+
+        public static List<Peak> GetAllPeaks()
+        {
+            return _allPeaks ?? (_allPeaks = ReadFromFile());
+        }
+
+        private static List<Peak> ReadFromFile()
+        {
+            const string filePath = "Data/peaks";
+            using (var reader = new StreamReader(filePath))
+            {
+                var json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<List<Peak>>(json);
+            }
+        }
+    }
+}
